32fc03150b1db27e1fb08f355180624cd4a80691
Teerark Sith <teerarksith@gmail.com>
2023-06-19 01:37:22 -0500

    a

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function multiply (a, b) {
     return a * b;
 }
 function divide (a, b) {
+    if(b == 0){
+        return "can't divide by 0";
+    }
     return a / b;
 }
 
@@ -33,24 +36,51 @@ function resetDisplay(){
     document.querySelector("#display").textContent = "";
 }
 
+function operatorClicked(){
+    let operators = document.querySelectorAll('.operator');
+    operators.forEach((item) =>{
+        if(item.classList.contains('clicked')){
+            item.classList.toggle('clicked');
+            return true;
+        }
+    });
+}
+
 let firstNumber = 0;
 let secondNumber = 0;
+let previousSum = 0;
 let operator = "";
 
 const numbers = document.querySelectorAll('.number');
-numbers.forEach(button => button.addEventListener('click', () => populateDisplay(button.textContent)) );
+numbers.forEach(button => button.addEventListener('click', () => {
+    populateDisplay(button.textContent);
+
+    if(operatorClicked())[
+        
+    ]
+}) );
 
 const operators = document.querySelectorAll('.operator');
 operators.forEach(button => button.addEventListener('click', () => {
-    populateDisplay(button.textContent)
-    operator = button.textContent}) );
+    firstNumber = document.querySelector('#display').textContent;
+    button.classList.add('clicked');
+    resetDisplay();
+    operator = button.textContent;
+}) );
 
 
 const equalsButton = document.querySelector('#equal');
 equalsButton.addEventListener('click', () =>{
-    const arr = document.querySelector('#display').textContent.split(operator);
-    firstNumber = arr[0];
-    secondNumber = arr[1];
+    secondNumber = document.querySelector('#display').textContent;
     resetDisplay();
-    populateDisplay(operate(+firstNumber,+secondNumber, operator));
+    console.log("first : "+firstNumber, "second: "+secondNumber, operator);
+    previousSum = operate(+firstNumber,+secondNumber, operator);
+    populateDisplay(previousSum);
+});
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    resetDisplay()
+    firstNumber = 0;
+    secondNumber = 0;
 });
